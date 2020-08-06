492f544fe0690057c281313188da7a6e9dfb550f
YerinCho <forjustice9839@gmail.com>
2020-08-06 18:16:37 +0900

    feat: 경로 조회에 대한 문서화

--- src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
+++ src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
@@ -1,4 +1,86 @@
 package wooteco.subway.maps.map.documentation;
 
-public class PathDocumentation {
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.google.common.collect.Lists;
+import wooteco.security.core.TokenResponse;
+import wooteco.subway.common.documentation.Documentation;
+import wooteco.subway.maps.map.application.MapService;
+import wooteco.subway.maps.map.domain.PathType;
+import wooteco.subway.maps.map.dto.PathResponse;
+import wooteco.subway.maps.map.ui.MapController;
+import wooteco.subway.maps.station.dto.StationResponse;
+
+@WebMvcTest(controllers = {MapController.class})
+public class PathDocumentation extends Documentation {
+    protected TokenResponse tokenResponse;
+    @Autowired
+    MapController mapController;
+    @MockBean
+    MapService mapService;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
+        super.setUp(context, restDocumentation);
+        tokenResponse = new TokenResponse("token");
+    }
+
+    @Test
+    void findPath() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("source", 1L);
+        params.put("target", 2L);
+        params.put("type", PathType.DISTANCE);
+
+        PathResponse pathResponse = new PathResponse(Lists.newArrayList(
+            new StationResponse(1L, "잠실역", LocalDateTime.now(), LocalDateTime.now()),
+            new StationResponse(2L, "잠실나루역", LocalDateTime.now(), LocalDateTime.now())
+        ), 10, 10, 100);
+
+        when(mapService.findPath(anyLong(), anyLong(), any())).thenReturn(pathResponse);
+
+        given().log().all().
+            header("Authorization", "Bearer " + tokenResponse.getAccessToken()).
+            accept(MediaType.APPLICATION_JSON_VALUE).
+            param("source", 1L).
+            param("target", 2L).
+            param("type", PathType.DISTANCE).
+            when().
+            get("/paths").
+            then().
+            log().all().
+            apply(document("paths/find-path",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                requestHeaders(
+                    headerWithName("Authorization").description("Bearer auth credentials")),
+                responseFields(
+                    fieldWithPath("stations[0].id").type(JsonFieldType.NUMBER).description("경로 역의 id"),
+                    fieldWithPath("stations[0].name").type(JsonFieldType.STRING).description("경로 역의 이름"),
+                    fieldWithPath("duration").type(JsonFieldType.NUMBER).description("소요 시간"),
+                    fieldWithPath("distance").type(JsonFieldType.NUMBER).description("소요 거리"),
+                    fieldWithPath("fare").type(JsonFieldType.NUMBER).description("요금")))).
+            extract();
+    }
+
 }
